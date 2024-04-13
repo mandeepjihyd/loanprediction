26d199f88721ec0d25125c388327ca58df759a37
mandeepjihyd <129370317+mandeepjihyd@users.noreply.github.com>
2024-04-13 09:10:27 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-file_path = r'C:\Users\DELL\mandeeproject\newmodel\loan_status_model.pkl'
-try:
-    with open(file_path, 'rb') as file:
-        model = pickle.load(file)
-    print("Model loaded successfully in test.")
-except Exception as e:
-    print(f"Test failed: {e}")
